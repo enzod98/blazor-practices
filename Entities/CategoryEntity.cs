717a106232e3cb98187a2eec3219b48bc4d19eeb
Enzo <enzod98@hotmail.com>
2023-03-22 21:32:52 -0300

    first commit

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    public class CategoryEntity
+    {
+        [Key]
+        public string CategoryId { get; set; }
+        public int CategoryName { get; set; }
+
+    }
+}

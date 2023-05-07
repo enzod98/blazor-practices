54f43789b37eabfc8f2801c60927bc205f73aa82
Enzo <enzod98@hotmail.com>
2023-05-07 02:32:37 -0400

    añadidas migraciones y estructuras de BD

---
+++ DataAccess/InventaryContext.cs
@@ -0,0 +1,69 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class InventaryContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<StorageEntity> Storages{ get; set; }
+        public DbSet<InputOutputEntity> InOuts{ get; set; }
+        public DbSet<WarehouseEntity> Warehouses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server=.\\SQLEXPRESS; Database=InventoryDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CategoryEntity>().HasData(
+                new CategoryEntity
+                {
+                    CategoryId = "ASH",
+                    CategoryName = "Aseo del hogar"
+                },
+                new CategoryEntity
+                {
+                    CategoryId = "PRF",
+                    CategoryName = "Perfumería"
+                },
+                new CategoryEntity
+                {
+                    CategoryId = "VDJ",
+                    CategoryName = "Videojuegos"
+                }
+            );
+
+            modelBuilder.Entity<WarehouseEntity>().HasData(
+                new WarehouseEntity
+                {
+                    WarehouseId = Guid.NewGuid().ToString(),
+                    WarehouseName = "Bodega central",
+                    WarehouseAddress = "Artigas 1945"
+                },
+                 new WarehouseEntity
+                 {
+                     WarehouseId = Guid.NewGuid().ToString(),
+                     WarehouseName = "Bodega norte",
+                     WarehouseAddress = "Abasto norte"
+                 },
+                  new WarehouseEntity
+                  {
+                      WarehouseId = Guid.NewGuid().ToString(),
+                      WarehouseName = "Bodega del Este",
+                      WarehouseAddress = "Ciudad del este"
+                  }
+            );
+        }
+    }
+}
--- Entities/CategoryEntity.cs
+++ Entities/CategoryEntity.cs
@@ -8,8 +8,14 @@ namespace Entities
     public class CategoryEntity
     {
         [Key]
+        [StringLength(50)]
         public string CategoryId { get; set; }
-        public int CategoryName { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+
+        public ICollection<ProductEntity> Products { get; set; }
 
     }
 }
